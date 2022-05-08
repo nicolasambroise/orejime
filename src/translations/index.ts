fa7acb1206ac8117cceae6d31bbe295b52944d04
Félix Girault <felix.girault@gmail.com>
2022-05-08 23:09:56 +0200

    TypeScript rewrite

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -1,6 +1,7 @@
 // To create a new translation, add a YAML file with the required entries and
 // link it here using the appropriate language code.
 
+import {Translations} from '../types';
 import de from './de.yml';
 import en from './en.yml';
 import fr from './fr.yml';
@@ -12,13 +13,13 @@ import nl from './nl.yml';
 import oc from './oc.yml';
 
 export default {
-	de,
-	en,
-	fr,
-	hu,
-	it,
-	ro,
-	nb,
-	nl,
-	oc
+	de: de as Translations,
+	en: en as Translations,
+	fr: fr as Translations,
+	hu: hu as Translations,
+	it: it as Translations,
+	ro: ro as Translations,
+	nb: nb as Translations,
+	nl: nl as Translations,
+	oc: oc as Translations
 };
